60bc5515cb8bfb516f1699f4ffc783c2022968fd
irawanyodi <dykhazta@gmail.com>
2025-01-08 14:38:51 +0700

    create: UI tombol set saldo awal - done

--- resources/views/livewire/report/temporary-report.blade.php
+++ resources/views/livewire/report/temporary-report.blade.php
@@ -11,16 +11,17 @@
             <h4>Tabungan</h4>
             <p class="text-base-content font-bold">Rp {{ number_format($savings, 0, ',', '.') }}</p>
         </div>
+
         <div>
             <h4>Total Pengeluaran</h4>
             <input type="number" wire:model="totalOutcome" class="input input-bordered w-full max-w-32 rounded-md" />
-
-            {{-- <p class="text-base-content font-bold">Rp {{ number_format($totalOutcome, 0, ',', '.') }}</p> --}}
         </div>
+
         <div>
             <h4>Saldo</h4>
             <p class="text-base-content font-bold">Rp {{ number_format($balance, 0, ',', '.') }}</p>
         </div>
+
         <div>
             <label class="form-control w-full max-w-xs">
                 <div class="label">
@@ -29,6 +30,11 @@
                 <input type="number" wire:model="savings" class="input input-bordered w-full max-w-32 rounded-md" />
             </label>
         </div>
+
+        <div>
+            <button class="btn btn-sm btn-neutral rounded-md text-base-100">Set Saldo Awal</button>
+        </div>
+
     </div>
 
     <button wire:click="generateReport" class="btn btn-neutral text-base-100 mt-4 rounded-md">
